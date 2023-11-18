b1bba6c9ca1c9b11143fd87c4365a32cabc69ecd
Tokhirjon <tohirsobirjonov8@gmail.com>
2023-11-18 18:34:31 +0500

    Registan

---
+++ fun.js
@@ -0,0 +1,8 @@
+const btn = document.getElementById('themeToggle').addEventListener('click', function() {
+    const currentTheme = document.body.className;
+    if (currentTheme === 'light-theme') {
+        document.body.className = 'dark-theme';
+    } else {
+        document.body.className = 'light-theme';
+    }
+});
